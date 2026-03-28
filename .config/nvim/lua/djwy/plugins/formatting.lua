7b75cf4a4890fd12bad29daff5118f10b779adb2
David Yuen <djwy@users.noreply.github.com>
2026-03-28 16:51:02 -0700

    Add biome formatters, FormatDisable/Enable commands, and fix format-on-save toggle

--- .config/nvim/lua/djwy/plugins/formatting.lua
+++ .config/nvim/lua/djwy/plugins/formatting.lua
@@ -9,33 +9,54 @@ return {
         css = { "prettier" },
         graphql = { "prettier" },
         html = { "prettier" },
-        javascript = { "prettier" },
-        javascriptreact = { "prettier" },
-        json = { "prettier" },
+        javascript = { "biome", "prettier", stop_after_first = true },
+        javascriptreact = { "biome", "prettier", stop_after_first = true },
+        json = { "biome", "prettier", stop_after_first = true },
+        jsonc = { "biome" },
         liquid = { "prettier" },
         lua = { "stylua" },
-        markdown = { "prettier" },
-        python = { "isort", "black" },
+        -- markdown = { "prettier" },
+        -- python = { "isort", "black" },
         ruby = { "rubocop" },
         scss = { "prettier" },
         svelte = { "prettier" },
-        typescript = { "prettier" },
-        typescriptreact = { "prettier" },
+        typescript = { "biome", "prettier", stop_after_first = true },
+        typescriptreact = { "biome", "prettier", stop_after_first = true },
         yaml = { "prettier" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { lsp_fallback = true, async = false, timeout_ms = 5000 }
+      end,
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
       conform.format({
         lsp_fallback = true,
         async = false,
-        timeout_ms = 1000,
+        timeout_ms = 2500,
       })
     end, { desc = "Format file or range (in visual mode)" })
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
   end,
 }
